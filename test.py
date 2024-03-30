b1e966c9ef68973d5fdeb4bf24bec17d4dbe205f
delitaputri <165451716+delitaputri@users.noreply.github.com>
2024-03-30 11:29:49 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.write('Test Streamlit')
+st.write('Random Test')
+st.title('Training Hari Ini')
+st.write('Hello, *Aceh!* :sunglasses:')
+st.header('Aplikasi Penjumlahan Numerik', divider='rainbow')
+
+angka_pertama=st.number_input('masukkan angka pertama')
+st.write('the first number is', angka_pertama)
+
+angka_kedua=st.number_input('masukkan angka kedua')
+st.write('the second number is', angka_kedua)
+
+operasi_matematika = angka_pertama + angka_kedua
+st.write(f"angka pertama {angka_pertama} x angka kedua {angka_kedua} = {operasi_matematika}")
